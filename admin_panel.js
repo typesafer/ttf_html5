4949a12cee372566927bbeb9f3e12c8a172f84b2
thiswolf <thismuller@gmail.com>
2014-02-17 13:04:58 +0300

    Update admin_panel.js

--- admin_panel.js
+++ admin_panel.js
@@ -8,7 +8,7 @@ $('.partner_create_button').on('click',function(){
 $('<article class="crud_item"><span class="delete_partner">Delete Partner</span><br/><label class="name_label">Partner Name</label><input class="text_element" type="text" /><label class="name_label">Partner URL</label><input class="text_element" type="text" /><label class="name_label">Partner Logo</label><input  class="text_element"type="text" /><span class="get_image_from_the_media_centre">Get Image From Media Centre</span><label class="name_label">Partner Status</label><select class="text_element"><option>Published</option><option>Pending Approval</option><option>In Future</option></select></article>').appendTo('.partners_holder');
 });
 //Delete Partner
-$('.delete_partner').on('click',function(){
+$(document).on('click', '.delete_partner', function(){
 alert('delete partner');
 });
 //Add Sidebar
