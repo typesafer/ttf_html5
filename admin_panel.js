c05f8c1d30b9bb0ab2697e71b12ac23a195d91fe
thiswolf <thismuller@gmail.com>
2014-02-17 15:01:08 +0300

    Update admin_panel.js

--- admin_panel.js
+++ admin_panel.js
@@ -8,8 +8,11 @@ $('.partner_create_button').on('click',function(){
 $('<article class="crud_item"><span class="delete_partner">Delete Partner</span><br/><label class="name_label">Partner Name</label><input class="text_element" type="text" /><label class="name_label">Partner URL</label><input class="text_element" type="text" /><label class="name_label">Partner Logo</label><input  class="text_element"type="text" /><span class="get_image_from_the_media_centre">Get Image From Media Centre</span><label class="name_label">Partner Status</label><select class="text_element"><option>Published</option><option>Pending Approval</option><option>In Future</option></select></article>').appendTo('.partners_holder');
 });
 //Delete Partner
-$(document).on('click', '.delete_partner', function(){
-alert('delete partner');
+ $('.partners_holder').on('click','.delete_partner',function() {
+    $(this).closest('.crud_item').fadeOut("slow", function()
+      {
+         $(this).remove();
+      });
 });
 //Add Sidebar
 $('.sidebar_create_button').on('click',function(){
@@ -17,8 +20,11 @@ $('<article class="crud_sidebar"><span class="delete_sidebar">Delete Sidebar</sp
 });
 //Delete Sidebar
 
-$(document).on('click', '.delete_sidebar', function(){
-         $(this).closest('.sidebars_holder').remove();
+ $('.sidebars_holder').on('click','.delete_sidebar',function() {
+    $(this).closest('.crud_sidebar').fadeOut("slow", function()
+      {
+         $(this).remove();
+      });
 });
 
 /**
