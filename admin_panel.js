7ffe680f4f16ccf7f3304cafa822868c28b4a403
thiswolf <thismuller@gmail.com>
2014-02-17 13:56:28 +0300

    Update admin_panel.js

--- admin_panel.js
+++ admin_panel.js
@@ -17,7 +17,7 @@ $('<article class="crud_sidebar"><span class="delete_sidebar">Delete Sidebar</sp
 });
 //Delete Sidebar
 
-$('.delete_sidebar').on('click', function(){
+$(document).on('click', '.delete_sidebar', function(){
          $(this).closest('sidebars_holder').remove();
 });
 
